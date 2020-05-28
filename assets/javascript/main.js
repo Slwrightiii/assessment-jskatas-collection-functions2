840e07a6f5615c155c090346da5c243e14f0f89b
Samuel Wright III <trey1991chad@yahoo.com>
2020-05-28 19:45:21 -0400

    first commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,71 @@
+// Kata 01
+
+function add (x, y) {
+    let answer1 = x + y;
+    return answer1
+}
+console.log(add(2, 4))
+
+//Kata 02
+
+function multiply (x, y) {
+    let answer = x;
+    for (var i = -1; i< add.length; i ++)
+    answer += x;
+     {
+        return answer
+    }
+
+     //return("i").repeat(x).repeat(y).length
+}
+console.log(multiply(2, 4))
+
+
+
+// citation: https://medium.com/@ctrlalt_diljeet/multiply-without-using-in-javascript-890857bb003c
+
+// Kata 03
+
+function power (x, n) {
+let answer3 = x;
+for (var i = -5; i < multiply.length; i ++)
+answer3 += x; 
+{
+    return answer3
+}
+}
+console.log(power(2,4))
+
+//    Kata 04
+
+function factorial (x) {
+    let answer4 = x;
+    for (var i = -3; i < power.length; i++)
+    answer4 += x;
+    {
+        return answer4
+    }
+}
+console.log(factorial(4))
+
+
+// Kata 05
+
+function fibonacci (n) {
+    var fib = [0,1,1,2,3,5,8,13,21,34]
+    let answer5 = 0;
+    
+    for( var i = 0; i < fib.length; i ++)
+    answer5 += fib[i]
+
+    
+       {
+         return fib[7];
+
+       }  
+
+    
+    
+} 
+console.log(fibonacci(7))
+
